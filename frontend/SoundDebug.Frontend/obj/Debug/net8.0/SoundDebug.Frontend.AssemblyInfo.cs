e6f532e1482cae50bb492628598479b92616dea6
izhdenevav <vizdeneva@gmail.com>
2026-04-01 12:57:09 +0700

    Fix: add Profile page

--- frontend/SoundDebug.Frontend/obj/Debug/net8.0/SoundDebug.Frontend.AssemblyInfo.cs
+++ frontend/SoundDebug.Frontend/obj/Debug/net8.0/SoundDebug.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundDebug.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eba75ecb085aef521960076a3f27e23bc62ab3d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f78d61927456583ec5bdc73509b0fcfe51ed5ac6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundDebug.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundDebug.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
